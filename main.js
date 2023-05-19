4b7e09696c03fd16212820db1f5da42d80f68ee9
gaby <gabgabrielligabriel@gmail.com>
2023-05-19 00:31:44 -0300

    asas

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+
+class Nodo{
+construnctor(value){
+    this.value=value;
+    this.raght=null ;
+    this.left=null ;
+  }
+
+}
+
+const p = document.querySelector('#pId');
+console.log(p.innerHtml)
