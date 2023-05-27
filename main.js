fcc495341ed63eabd51258942283179d996a482f
gaby <gabgabrielligabriel@gmail.com>
2023-05-27 13:48:20 -0300

    as

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 
 
-
-class Nodo{
-construnctor(value){
-    this.value=value;
-    this.raght=null ;
-    this.left=null ;
-  }
-
-}
-
 const p = document.querySelector('#pId');
-console.log(p.innerHtml)
+console.log(p.innerHtml) // Va a imprimir el texto dentro del párrafo con el id 'pID'
+p.innerHTML = 'Nuevo texto'; // Acá estamos modificando el texto del párrafo mencionado anteriormente
+console.log(p.innerHTML); // Va a imprimir el nuevo texto que le seteamos, es decir: "Nuevo texto"
